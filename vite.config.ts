f2a51e3145835c9b6f0951c667bdd512d39fd712
Coldqh <pahomov.0666@gmail.com>
2026-05-16 00:21:00 +0300

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/friday-night-dynasty/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
   },
   test: {
     environment: 'node',
-    globals: true
-  }
+    globals: true,
+  },
 });
